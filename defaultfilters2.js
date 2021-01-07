99237c1dfc3fd46fe46601dbec2f459d68f5b59a
Eric Chen <ericfhchen@gmail.com>
2021-01-07 13:22:13 -0500

    2

---
+++ defaultfilters2.js
@@ -0,0 +1,54 @@
+// Filter on Page Load
+(function defaultFilters() {
+    // HIRING
+    document.querySelector('.filter-btn-school').click();
+    document.querySelector('.filter-btn-year').click();
+    
+    document.querySelector('.filter-btn-black-african').click(); 
+    document.querySelector('.filter-btn-indigenous-native').click(); 
+    document.querySelector('.filter-btn-caucasian').click(); 
+    document.querySelector('.filter-btn-south-asian').click(); 
+    document.querySelector('.filter-btn-middle-eastern').click(); 
+    document.querySelector('.filter-btn-east-southeast-asian').click(); 
+    document.querySelector('.filter-btn-latinx-hispanic').click(); 
+
+    document.querySelector('.filter-btn-gender').click();
+    document.querySelector('.filter-btn-lgbtq').click();
+    document.querySelector('.filter-btn-disabled').click();
+    
+
+    // SCHOLARSHIPS
+    document.querySelector('.filter-btn-school2').click();
+    document.querySelector('.filter-btn-department2').click();
+    document.querySelector('.filter-btn-year2').click();
+    
+    document.querySelector('.filter-btn-black-african2').click(); 
+    document.querySelector('.filter-btn-indigenous-native2').click(); 
+    document.querySelector('.filter-btn-caucasian2').click(); 
+    document.querySelector('.filter-btn-south-asian2').click(); 
+    document.querySelector('.filter-btn-middle-eastern2').click(); 
+    document.querySelector('.filter-btn-east-southeast-asian2').click(); 
+    document.querySelector('.filter-btn-latinx-hispanic2').click(); 
+
+    document.querySelector('.filter-btn-gender2').click();
+    document.querySelector('.filter-btn-lgbtq2').click();
+    document.querySelector('.filter-btn-disabled2').click();
+    
+
+    // EVENTS
+    document.querySelector('.filter-btn-school3').click();
+    document.querySelector('.filter-btn-department3').click();
+    document.querySelector('.filter-btn-year3').click();
+    
+    document.querySelector('.filter-btn-black-african3').click(); 
+    document.querySelector('.filter-btn-indigenous-native3').click(); 
+    document.querySelector('.filter-btn-caucasian3').click(); 
+    document.querySelector('.filter-btn-south-asian3').click(); 
+    document.querySelector('.filter-btn-middle-eastern3').click(); 
+    document.querySelector('.filter-btn-east-southeast-asian3').click(); 
+    document.querySelector('.filter-btn-latinx-hispanic3').click(); 
+
+    document.querySelector('.filter-btn-gender3').click();
+    document.querySelector('.filter-btn-lgbtq3').click();
+    document.querySelector('.filter-btn-disabled3').click();
+  })();
